dfe31badae81beed169b48c036589052bf0d3d76
albatron34000 <82473077+albatron34000@users.noreply.github.com>
2024-04-28 09:53:35 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,47 +1,42 @@
 #!/bin/bash
-function compile()
-{
-source ~/.bashrc && source ~/.profile
-export LC_ALL=C && export USE_CCACHE=1
-ccache -M 25G
-export ARCH=arm64
-export KBUILD_BUILD_HOST=android-build
-export KBUILD_BUILD_USER="kardebayan"
-clangbin=clang/bin/clang
-if ! [ -a $clangbin ]; then git clone --depth=1 https://github.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-6443078 clang
-fi
-gcc64bin=gcc64/bin/aarch64-linux-android-as
-if ! [ -a $gcc64bin ]; then git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 gcc64
-fi
-gcc32bin=gcc32/bin/arm-linux-androideabi-as
-if ! [ -a $gcc32bin ]; then git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 gcc32
-fi
-rm -rf AnyKernel
-make O=out ARCH=arm64 RM6785_defconfig
-PATH="${PWD}/clang/bin:${PATH}:${PWD}/gcc32/bin:${PATH}:${PWD}/gcc64/bin:${PATH}" \
-make -j$(nproc --all) O=out \
-                      ARCH=arm64 \
-                      CC="clang" \
-                      CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE="${PWD}/gcc64/bin/aarch64-linux-android-" \
-                      CROSS_COMPILE_ARM32="${PWD}/gcc32/bin/arm-linux-androideabi-" \
-                      LD=ld.lld \
-                      CONFIG_NO_ERROR_ON_MISMATCH=y
+
+# Copyright (C) 2024 psionicprjkt
+
+compile_kernel() {
+    # compile_kernel
+    export ARCH=arm64
+    make O=out ARCH=arm64 RM6785_defconfig
+
+    # Generate profile data during the first compilation
+    PATH="${PWD}/clang/bin:${PWD}/arm64:${PWD}/arm32:${PATH}" \
+    make -j"$(nproc --all)" O=out \
+        LLVM=1 \
+        LLVM_IAS=1 \
+        ARCH=arm64 \
+        CC="clang" \
+        LD=ld.lld \
+        STRIP=llvm-strip \
+        AS=llvm-as \
+        AR=llvm-ar \
+        NM=llvm-nm \
+        OBJCOPY=llvm-objcopy \
+        OBJDUMP=llvm-objdump \
+        CLANG_TRIPLE=aarch64-linux-gnu- \
+        CROSS_COMPILE="${PWD}/arm64/aarch64-linux-android-" \
+        CROSS_COMPILE_ARM32="${PWD}/arm32/arm-linux-androideabi-" \
+        CONFIG_NO_ERROR_ON_MISMATCH=y \
+        CFLAGS="-Wno-pragma-messages"
 }
-function zupload()
-{
-zimage=out/arch/arm64/boot/Image.gz
-if ! [ -a $zimage ];
-then
-echo  " Failed To Compile Kernel"
-else
-echo -e " Kernel Compile Successful"
-git clone --depth=1 https://github.com/kardebayan/AnyKernel3.git AnyKernel
-cp out/arch/arm64/boot/Image.gz AnyKernel
-cd AnyKernel
-zip -r9 Stormbreaker-RMX2001L1-v1.zip *
-cd ../
-fi
+
+setup_kernel_release() {
+    # setup_kernel_release
+    v=$(cat version)
+    d=$(date "+%d%m%Y")
+    z="storm-kernel-RM6785-$d-$v-ksu.zip"
+    wget --quiet https://psionicprjkt.my.id/assets/files/AK3-salaa.zip && unzip AK3-salaa
+    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel && cd AnyKernel
+    zip -r9 "$z" *
 }
-compile
-zupload
+
+compile_kernel
+setup_kernel_release
